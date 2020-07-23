e16080bb275f64e9860a7c7700f1af72be080b6e
Leewansze <1226186467@qq.com>
2020-07-24 02:52:18 +0800

    [GuessNumber][Alice]feature: implement Main to play guessnumber game

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,28 @@
+package example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AnswerGeneratorImp answerGeneratorImp = new TestAnswerGenerator();
+        GuessNumber guessNumber = new GuessNumber();
+        int []answer = answerGeneratorImp.answerNumber();
+
+
+        int guess[] = new int[4];
+        int countInputTime = 0;
+        while(sc.hasNextShort()){
+            for(int i = 0; i < guess.length; i++){
+                guess[i] = sc.nextInt();
+            }
+            countInputTime++;
+            if(countInputTime == 6){
+                break;
+            }
+            System.out.println(guessNumber.guess(guess, answer));
+        }
+        System.out.printf("The answer is %s.\n", answer);
+
+    }
+}
