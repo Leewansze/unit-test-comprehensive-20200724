fa749ced23e8692b5aa4eb52af45abd440726bc1
Leewansze <1226186467@qq.com>
2020-07-24 00:52:25 +0800

    [GuessNumber][Alice]test: should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234

---
+++ src/test/java/example/GuessNumberTest.java
@@ -0,0 +1,23 @@
+package example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GuessNumberTest {
+    @Test
+    void should_return_4A0B_when_guess_given_answer_1234_and_input_guess_1234() {
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        int[] guessNum = {1,2,3,4};
+        int[] answer = {1,2,3,4};
+
+        //when
+        String result = guessNumber.guess(guessNum, answer);
+
+        //then
+        assertEquals("4A0B",result);
+
+
+    }
+}
